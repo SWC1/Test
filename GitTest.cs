0ce1b8b6b0fca563d301e7a69ee1ea85e0e2e64d
heho <heho@easj.dk>
2014-11-12 15:37:33 +0100

    init

---
+++ GitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestConsole
+{
+    class GitTest
+    {
+        public void m1()
+        {
+        }
+
+        public void m2() { }
+    }
+}
